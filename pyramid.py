c4c3073914766492865065aa62b2c103bff5ac25
Shane Rosanbalm <srosanba@gmail.com>
2018-12-05 15:18:42 -0500

    add csv

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+# subset data (e.g., indoor lead climbs in the past 12 months)
+
+# calculate counts by grade and style (e.g., seven 10b onsights)
+
+# path 1: create 8a.nu-style plot (i.e., show with everything)
+
+# path 2: create pyrmaid-style plots (caps, carryovers, etc.)
+
+
